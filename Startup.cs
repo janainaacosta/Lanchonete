9cc4768f86d9ced9182102be3792f18828e0a167
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-06-09 22:43:51 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,67 @@
+﻿using Lanchonete.Context;
+using Lanchonete.Repositories;
+using Lanchonete.Repositories.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lanchonete;
+
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // Este método é chamado pelo runtime. Use este método para adicionar serviços ao contêiner.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+
+        services.AddControllersWithViews();
+        // Adicione outros serviços conforme necessário
+    }
+
+    // Este método é chamado pelo runtime. Use este método para configurar o pipeline de requisições HTTP.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            // Adicione outros endpoints conforme necessário
+        });
+    }
+}
